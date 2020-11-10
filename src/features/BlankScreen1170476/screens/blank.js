cd78f367782e0a93ea8a2e721892ce756b04b471
Crowdbotics <team@crowdbotics.com>
2020-11-10 17:45:39 +0000

    Update components

--- src/features/BlankScreen1170476/screens/blank.js
+++ src/features/BlankScreen1170476/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,33 +8,54 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Text style={styles.Text_3}>This is Screen 1</Text>
+      <Button
+        title="Press me!"
+        color="#70fff5"
+        onPress={() => alert("Pressed!")}
+      />
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("BlankScreen0170474")}
+      >
+        <Icon name="chevron-circle-left" style={styles.Icon_43} />
+      </TouchableOpacity>
+      <Text>The above icon will navigate to screen 0 when pressed</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
+    marginHorizontal: 16
   },
-});
+  View_1: {},
+  Text_3: {
+    fontSize: 24,
+    color: "#07b8f2",
+    fontFamily: "Merriweather-Regular",
+    textAlign: "center"
+  },
+  Button_5: {},
+  Icon_43: { color: "#9916df" },
+  Text_45: {}
+})
